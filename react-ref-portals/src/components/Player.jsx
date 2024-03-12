7773b722f76c66a1fb24894e9d3b2714e41329c9
Wen <wbai@deltacontrols.com>
2024-03-11 20:53:11 -0700

    ref

---
+++ react-ref-portals/src/components/Player.jsx
@@ -0,0 +1,20 @@
+import { useState, useRef} from "react";
+
+export default function Player() {
+  const playerNameRef = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+
+  function handleClick() {
+    setEnteredPlayerName(playerNameRef.current.value);
+    playerNameRef.current.value = "";
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <p>
+        <input ref={playerNameRef} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
---
+++ react-ref-portals/src/components/TimerChallenge.jsx
@@ -0,0 +1,32 @@
+import { useState, useRef } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timerRef = useRef();
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  function handleStart() {
+    timerRef.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timerRef.current);
+  }
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You Lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className="">Time is running...</p>
+    </section>
+  );
+}
